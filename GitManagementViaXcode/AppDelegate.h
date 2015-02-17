0a83cb7e2b3064df35796f62b488ae23f79c23e3
Sedrix Caritero <Sedrix@Epicurus.local>
2015-02-17 17:14:53 +0800

    Initial Commit

---
+++ GitManagementViaXcode/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GitManagementViaXcode
+//
+//  Created by Sedrix Caritero on 2/17/15.
+//  Copyright (c) 2015 flightdigitalmedia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GitManagementViaXcode/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GitManagementViaXcode
+//
+//  Created by Sedrix Caritero on 2/17/15.
+//  Copyright (c) 2015 flightdigitalmedia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
